8059ae4aa4aa00efde509e6bb107d093eb3c15ad
YinQing <yinqing8899@sina.com>
2016-12-26 10:34:27 +0800

    Update page.js

--- Page/js/page.js
+++ Page/js/page.js
@@ -84,6 +84,7 @@
 		var _curr = curr;
 		var prevHide = curr===1?"hide":"";
 		var nextHide = curr===pages?"hide":"";
+		var firstHide,lastHide;
 		for (var i=0;i<pages;i++) {
 			if (curr-2<=2) {
 				if(pages>7 && i>=6){
@@ -93,7 +94,7 @@
 				}else{
 					li.push('<li><a href="#" data-page="'+(i+1)+'">'+(i+1)+'</a></li>');
 				}
-				prevHide = "hide";
+				firstHide = "hide";
 			}
 			else if (curr-2>2 && curr+2<pages-1) {
 				if(i===0){
@@ -131,7 +132,7 @@
 							default:
 								i = _curr-5;
 						}
-						nextHide = "hide";
+						lastHide = "hide";
 						_curr++;
 						li.push('<li><a href="#" data-page="'+(i+1)+'">'+(i+1)+'</a></li>');
 					}
@@ -143,12 +144,10 @@
 		liArr = li;
 			switch(params.style) {
 				case  1 :
-				liArr.unshift('<li><a class="prev-page '+prevHide+'" data-page="prev-page" title="上一页">上一页</a></li>'+
-						'<li><a class="first-page '+prevHide+'" data-page="first-page" title="首页">首页</a></li>'
-				);
-				liArr.push('<li><a class="last-page '+nextHide+'" data-page="last-page" title="尾页">尾页</a></li>'+
-						'<li><a class="next-page '+nextHide+'" data-page="next-page" title="下一页">下一页</a></li>'
-				);
+				liArr.unshift('<li><a class="first-page '+firstHide+'" data-page="first-page" title="首页">首页</a></li>');
+				liArr.unshift('<li><a class="prev-page '+prevHide+'" data-page="prev-page" title="上一页">上一页</a></li>')
+				liArr.push('<li><a class="last-page '+lastHide+'" data-page="last-page" title="尾页">尾页</a></li>');
+				liArr.push('<li><a class="next-page '+nextHide+'" data-page="next-page" title="下一页">下一页</a></li>');
 				break;
 				case 2 :
 				liArr.unshift('<li><a class="prev-page '+prevHide+'" data-page="prev-page" title="上一页">上一页</a></li>');
